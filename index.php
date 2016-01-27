a2c0c6fe972cf0a7ed070e21155998244d4f7dd3
koooyf <kyf19970408@163.com>
2016-01-27 09:01:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <title>koooyf</title>
-<?php
-error_reporting(E_ALL);
-$aa=$_POST['user'];
-?>
 <body>
-<form name="input" action="" method="post">
+<form  action="insert.php" method="post">
 Word: <br/>
 <textarea type="text" name="user"></textarea><br/><br/>
 <input type="submit" value="Submit" />
 </form>
-<h1> There is your record</h1>
-<ul>
-<?php
-echo "<li>$aa</li>";
-?>
-</ul>
