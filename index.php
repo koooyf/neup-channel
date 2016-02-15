ce2c5c5f0c84d07f1ee069625f130bb757e3c9aa
koooyf <kyf19970408@163.com>
2016-02-15 12:01:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<title>koooyf</title>
+<?php
+ session_start();
+$_SESSION["admin"]=null;
+?><title>koooyf</title>
 <body>
+<form  action="laod.php" method="post">
+<p>输入你的管理员账号和密码</p>
+account:
+<input type="text" name="account">
+<br/><br/>
+password:
+<input type="text" name="password">
+<input type="submit" value="Log in">
+<br/><br/>
+</form>
 <form  action="insert.php" method="post">
-Word: <br/>
+留言框： <br/>
 <textarea type="text" name="user"></textarea><br/><br/>
 <input type="submit" value="Submit" />
+</form>
+<br/>
+<form action="cookie.php" method="post">
+<input type="submit" value="点我获取饼干">
 </form>
