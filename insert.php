49a4a4a7713d8588f3f34701ef916db9d2788df0
koooyf <kyf19970408@163.com>
2016-02-15 12:03:01 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,14 +5,23 @@
 	  die('Could not connect: '.mysql_error());
   }
   mysql_select_db("talk", $shuru);
+ $sql="select * from binggan where cookie='$_COOKIE[mycookie]'";
+ $rs=mysql_fetch_array($sql);
+if($rs)
+{
   $hh="insert into information(say)
-	  VALUES('$_POST[user]')";
+	  VALUES('$_COOKIE[mycookie]:$_POST[user]')";
   if(!mysql_query($hh,$shuru))
     {
     die('error :'.mysql_error()); 
     }
-  mysql_close($shuru)
+}
+else echo "你没有饼干快去领饼干";
+  mysql_close($shuru);
  ?>
 <form action="input.php" method="post">
 <input type="submit" value="look your records"/>
+</form>
+<form action="index.php" meth="post">
+<input type="submit" value="返回">
 </form>
